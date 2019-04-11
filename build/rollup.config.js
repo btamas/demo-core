1495dc11a3a2762e80d2e480e43c90ee6463c31b
Tamas Besenyei <tamas@besenyei.net>
2019-04-11 09:43:38 +0200

    add rollup support

---
+++ build/rollup.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const glob = require('glob');
+const alias = require('rollup-plugin-alias');
+
+const inputs = glob.sync(path.join(__dirname, '..', 'src', '**', '*.js'));
+const localExternals = inputs.map(input => `ui/${input.replace(/^.*?src\//, '').replace(/\.js/, '')}`);
+console.log(localExternals);
+
+module.exports = inputs.map(input => {
+    const name = path.parse(input).name.replace(/\.js/, '');
+    const dir = path.parse(input).dir.replace(/^.*?src/, '');
+    
+    return {
+        input,
+        output: {
+            dir: path.join(__dirname, '..', 'dist', 'js', dir),
+            format: 'umd',
+            name
+        },
+        external: [
+        ].concat(localExternals),
+        plugins: [
+            alias({
+                resolve: ['.js', '.css', '.tpl'],
+                core: path.join(__dirname, '..', 'src')
+              }),
+            //   uglify()
+        ]
+    };
+});
